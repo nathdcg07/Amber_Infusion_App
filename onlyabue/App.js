755028a9149b33a644c1ea16299024a099ce693f
JessDuff <jess.tasstic@gmail.com>
2024-10-01 22:23:06 -0400

    Merge backend y frontend

--- onlyabue/App.js
+++ onlyabue/App.js
@@ -1,7 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect } from 'react';
+
+import initializeCollections from './services/initFirestore';
 
 export default function App() {
+  
+  useEffect(() => {
+    const init = async () => {
+      try {
+        await initializeCollections();
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    init();
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
---
+++ onlyabue/services/firebaseConfig.js
@@ -0,0 +1,25 @@
+// import firebase from 'firebase/app';
+// import 'firebase/firestore';
+import { initializeApp } from 'firebase/app'; // Import from firebase/app
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyA1eVXH6MoW-HdH0ENnPF9iE47IQqeEm70",
+    authDomain: "abueapp-d3758.firebaseapp.com",
+    databaseURL: "https://abueapp-d3758-default-rtdb.firebaseio.com",
+    projectId: "abueapp-d3758",
+    storageBucket: "abueapp-d3758.appspot.com",
+    messagingSenderId: "619524224303",
+    appId: "1:619524224303:web:1f36ff21f93f7c568c5dda",
+    measurementId: "G-FDVRKXDNGQ"
+  };
+
+// if (!firebase.apps.length) {
+//   firebase.initializeApp(firebaseConfig);
+// }
+
+const app = initializeApp(firebaseConfig);
+const firestore = getFirestore(app); //
+
+// const firestore = firebase.firestore();
+export { firestore };
