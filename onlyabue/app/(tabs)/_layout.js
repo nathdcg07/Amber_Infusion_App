cc083782ca66c5d4aa2e8ae8089561c212bf9aa2
TheRots100 <wilfredo_oliver@hotmail.com>
2024-10-02 22:25:55 -0400

    Navbar completo,registro completo, vista alarmas completo

---
+++ onlyabue/app/(tabs)/_layout.js
@@ -0,0 +1,18 @@
+import {Tabs} from "expo-router";
+import { View } from "native-base";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { MaterialIcons } from "@expo/vector-icons";
+import Fontisto from '@expo/vector-icons/Fontisto';
+
+export default function TabsLayout(){
+
+    return(
+        <Tabs screenOptions={{tabBarActiveTintColor: 'white',tabBarInactiveTintColor: 'rgba(255, 255, 255, 0.6)',tabBarInactiveBackgroundColor: '#29B6F6',tabBarActiveBackgroundColor: '#29B6F6',headerShown:false, tabBarStyle:{ height: 70,}, tabBarLabelStyle: {
+            fontSize: 14,}, }} >
+            <Tabs.Screen name="home" options={{title: "Medicamentos", tabBarIcon: ({color})=> <Fontisto name="pills" size={30} color={color}/>,  }} />
+            <Tabs.Screen name="medicDates" options={{title: "Citas",tabBarIcon: ({color})=> <Fontisto name="doctor" size={30} color={color}/>, }}/>
+            <Tabs.Screen name="search" options={{title: "Buscar",tabBarIcon: ({color})=> <MaterialIcons name="search" size={30} color={color} />, }}/>
+            <Tabs.Screen name="account" options={{title: "Cuenta",tabBarIcon: ({color})=> <Fontisto name="male" size={30} color={color}/>,}}/>
+        </Tabs>
+    )
+}
