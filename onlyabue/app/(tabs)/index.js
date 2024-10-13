30b07537751957ed4790276437fa6b9b36802f22
JessDuff <jess.tasstic@gmail.com>
2024-10-13 13:32:36 -0400

    Eliminar codigo innecesario

--- onlyabue/app/(tabs)/index.js
+++ onlyabue/app/(tabs)/index.js
@@ -1,5 +1,3 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { NativeBaseProvider } from "native-base";
 import { View } from 'native-base';
 import { AlarmHome } from '../../components/alarmHome';
 
@@ -12,8 +10,6 @@ import { agregarMedicamento,
   actualizarMedicamento, 
   eliminarMedicamento } from '../../services/firestoreService';
 
-
-
 export default function Index() {
 
     useEffect(() => {
@@ -25,42 +21,6 @@ export default function Index() {
         }};
 
         init();
-    }, []);
-
-    useEffect(() => {
-        const agregarMedicamentoEjemplo = async () => {
-          try {
-            await agregarMedicamento({
-              nombre: 'Paracetamol',
-              dosis: '500mg',
-              usuarioId: 'usuario1234',
-              creadoEn: new Date(),
-            });
-            console.log('Medicamento agregado correctamente');
-          } catch (error) {
-            console.error('Error al agregar medicamento:', error);
-          }
-        };
-        const obtenerDatos = async () => {
-          const medicamentos = await obtenerMedicamentos();
-          console.log('Medicamentos:', medicamentos);
-        };
-    
-        const medicamentosPorUsuario = async () => {
-          const medicamentosUsuario = await obtenerMedicamentosPorUsuario('usuario123');
-          console.log('usuario123', medicamentosUsuario);
-        }
-        const actualizarMed = async () => {
-          actualizarMedicamento('PSDBxeQM9HdwtEjbdElz', { dosis: '1000mg' });
-        }
-    
-        const eliminarMed = async () => {
-          eliminarMedicamento('N5sIPyXh11e1I1r0rSQm');
-        }
-    
-        // eliminarMed();
-        agregarMedicamentoEjemplo();
-    
     }, []);
     
     return (
