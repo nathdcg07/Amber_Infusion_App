a3232abaed73fb266e2df7a471a3522e11de2c8e
nathdcg07 <nathalydcgutierrez@gmail.com>
2024-11-05 17:32:16 -0400

    ?

--- onlyabue/services/firestoreService.js
+++ onlyabue/services/firestoreService.js
@@ -256,3 +256,51 @@ export const actualizarEtiqueta = async (id, data) => {
 export const eliminarEtiqueta = async (id) => {
   await deleteDoc(doc(db, "etiquetas", id));
 };
+
+//para el token
+export const verificarToken = async (token) => {
+  try {
+    const usuariosRef = collection(firestore, 'Usuarios');
+    const q = query(usuariosRef, where('UserID', '==', token));
+    const querySnapshot = await getDocs(q);
+
+    return !querySnapshot.empty;
+  } catch (error) {
+    console.error("Error al verificar el token:", error);
+    return false;
+  }
+};
+
+//funciones para citas medicas crear, buscar, actualizar, borrar
+export const crearCitaMedica = async (cita) => {
+  try {
+    const docRef = await addDoc(collection(firestore, "citas_medicas"), cita);
+    console.log("Cita creada exitosamente", docRef.id);
+  } catch (error) {
+    console.error("Error al crear la cita:", error);
+  }
+};
+
+export const leerCitaMedica = async (id) => {
+  const docRef = doc(firestore, "citas_medicas", id);
+  const docSnap = await getDoc(docRef);
+
+  if (docSnap.exists()) {
+    return docSnap.data();
+  } else {
+    console.log("No se encontró la cita");
+    return null;
+  }
+};
+
+export const actualizarCitaMedica = async (id, nuevosDatos) => {
+  const docRef = doc(firestore, "citas_medicas", id);
+  await updateDoc(docRef, nuevosDatos);
+  console.log("Cita actualizada con éxito");
+};
+
+export const eliminarCitaMedica = async (id) => {
+  await deleteDoc(doc(firestore, "citas_medicas", id));
+  console.log("Cita eliminada con éxito");
+};
+
